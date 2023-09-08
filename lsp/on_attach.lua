ab8bf33de13038ca69333e73bbb6082dcbca5b85
takayukiooi <takayuki.oi@makuake.co.jp>
2023-09-08 22:11:08 +0900

    fix code action keybind

--- lsp/on_attach.lua
+++ lsp/on_attach.lua
@@ -8,7 +8,7 @@ return function(client, bufnr)
   map.set('n', 'K', '<cmd>Lspsaga show_line_diagnostics<cr>')
   map.set('n', 'J', '<cmd>Lspsaga signature_help<cr>')
   map.set('n', 'H', '<cmd>Lspsaga preview_definition<cr>')
-  map.set('n', '<space>.', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+  map.set('n', '<space>la', '<cmd>lua vim.lsp.buf.code_action()<cr>')
   map.set('n', '<space>lr', '<cmd>lua vim.lsp.buf.references()<cr>')
   map.set('n', '<space>lm', '<cmd>lua vim.lsp.buf.definition()<cr>')
 end
